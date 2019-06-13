d62821d6f4f80a23e382562c14afeedab2570b79
Blair Stevens <stevens.blair.e@gmail.com>
2019-06-13 10:37:13 -0400

    wprking blog test

---
+++ pelicanconf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Blair Stevens'
+SITENAME = 'Blog'
+SITEURL = 'https://blairstevens.github.io/Blog'
+PATH = 'content'
+STATIC_PATHS = ['images']
+TIMEZONE = 'America/Toronto'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
